9eb732450e469ec5e4796fc9915fbae507fc0b6d
yatinsingh2007 <yatin4591@gmail.com>
2025-10-23 02:26:41 +0530

    add google site verification meta tag to RootLayout

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="L63uGSjv-ig202O9O7OB6XKbgHRQHhHyiKmkxhoJjNw" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

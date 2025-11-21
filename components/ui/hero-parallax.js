c7590818fc776743455245b7c37bce998b751b2f
yatinsingh2007 <yatin4591@gmail.com>
2025-11-21 10:09:05 +0530

    hero parralax component fixed

--- components/ui/hero-parallax.js
+++ components/ui/hero-parallax.js
@@ -1,17 +1,22 @@
 "use client";
-
-import React, { useEffect } from "react";
-import { motion, useScroll, useTransform, useSpring } from "framer-motion";
+import React from "react";
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useSpring,
+  MotionValue,
+} from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 
-export const HeroParallax = ({ products }) => {
-
+export const HeroParallax = ({
+  products,
+}) => {
   const firstRow = products.slice(0, 5);
   const secondRow = products.slice(5, 10);
   const thirdRow = products.slice(10, 15);
   const ref = React.useRef(null);
-
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["start start", "end start"],
@@ -21,36 +26,34 @@ export const HeroParallax = ({ products }) => {
 
   const translateX = useSpring(
     useTransform(scrollYProgress, [0, 1], [0, 1000]),
-    springConfig
+    springConfig,
   );
   const translateXReverse = useSpring(
     useTransform(scrollYProgress, [0, 1], [0, -1000]),
-    springConfig
+    springConfig,
   );
   const rotateX = useSpring(
     useTransform(scrollYProgress, [0, 0.2], [15, 0]),
-    springConfig
+    springConfig,
   );
   const opacity = useSpring(
     useTransform(scrollYProgress, [0, 0.2], [0.2, 1]),
-    springConfig
+    springConfig,
   );
   const rotateZ = useSpring(
     useTransform(scrollYProgress, [0, 0.2], [20, 0]),
-    springConfig
+    springConfig,
   );
   const translateY = useSpring(
     useTransform(scrollYProgress, [0, 0.2], [-700, 500]),
-    springConfig
+    springConfig,
   );
-
   return (
     <div
       ref={ref}
-      className="h-[260vh] md:h-[200vh] xl:h-[320vh] py-40 overflow-hidden antialiased relative flex flex-col self-auto perspective-1000 preserve-3d"
+      className="h-[260vh] md:h-[200vh] xl:h-[320vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
     >
       <Header />
-
       <motion.div
         style={{
           rotateX,
@@ -58,22 +61,33 @@ export const HeroParallax = ({ products }) => {
           translateY,
           opacity,
         }}
+        className=""
       >
         <motion.div className="flex flex-row-reverse space-x-reverse space-x-20 mb-20">
           {firstRow.map((product) => (
-            <ProductCard product={product} translate={translateX} key={product.title} />
+            <ProductCard
+              product={product}
+              translate={translateX}
+              key={product.title}
+            />
           ))}
         </motion.div>
-
-        <motion.div className="flex flex-row mb-20 space-x-20">
+        <motion.div className="flex flex-row  mb-20 space-x-20 ">
           {secondRow.map((product) => (
-            <ProductCard product={product} translate={translateXReverse} key={product.title} />
+            <ProductCard
+              product={product}
+              translate={translateXReverse}
+              key={product.title}
+            />
           ))}
         </motion.div>
-
         <motion.div className="flex flex-row-reverse space-x-reverse space-x-20">
           {thirdRow.map((product) => (
-            <ProductCard product={product} translate={translateX} key={product.title} />
+            <ProductCard
+              product={product}
+              translate={translateX}
+              key={product.title}
+            />
           ))}
         </motion.div>
       </motion.div>
@@ -83,9 +97,10 @@ export const HeroParallax = ({ products }) => {
 
 export const Header = () => {
   return (
-    <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full left-0 top-0">
+    <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        Checkout my <br /> Projects
+        Checkout my <br />
+        Projects
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
         Created amazing user interfaces with React, making websites that look
@@ -95,25 +110,35 @@ export const Header = () => {
   );
 };
 
-export const ProductCard = ({ product, translate }) => {
+export const ProductCard = ({
+  product,
+  translate,
+}) => {
   return (
     <motion.div
-      style={{ x: translate }}
-      whileHover={{ y: -20 }}
+      style={{
+        x: translate,
+      }}
+      whileHover={{
+        y: -20,
+      }}
+      key={product.title}
       className="group/product h-96 w-[30rem] relative flex-shrink-0"
     >
-      <Link href={product.link} className="block group-hover/product:shadow-2xl" target="_blank">
+      <Link
+        href={product.link}
+        className="block group-hover/product:shadow-2xl "
+        target="_blank"
+      >
         <Image
           src={product.thumbnail}
-          height={600}
-          width={600}
+          height="600"
+          width="600"
           className="object-cover object-left-top absolute h-full w-full inset-0"
           alt={product.title}
         />
       </Link>
-
       <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none"></div>
-
       <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white">
         {product.title}
       </h2>
